5c20f0ccbd0868456f0b8851f2446b520949c230
Pavel314 <ivonov_pavel@mail.ru>
2024-03-23 03:46:24 +0300

    Добавлена гр. библиотека

---
+++ PLNCompiler/doc/GRABC.pas
@@ -0,0 +1,19 @@
+﻿library GRABC;
+uses GraphABC;
+begin
+MoveTo(0,0);
+Circle(100,100,100);
+Line(0,0,0,0);
+SetPixel(0,0,clRed);
+Rectangle(0,0,0,0);
+SaveWindow('asd');
+WindowWidth();
+WindowHeight();
+GraphABC.LoadWindow('asd');
+GraphABC.SetBrushColor(Color.Red);
+GraphABC.SetPenColor(Color.Red);
+GraphABC.SetPenWidth(1);
+GraphABC.LineRel(0,0);
+LockDrawing();
+Redraw();
+end.
